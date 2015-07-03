aa07263cac344ca0892d6b0d0b50a2462b709a1d
piercez <pierce@growingcoder.com>
2015-07-03 17:21:52 -0600

    Setup menu for country picker.

--- app/src/main/java/com/growingcoder/spotifystreamer/search/MainActivity.java
+++ app/src/main/java/com/growingcoder/spotifystreamer/search/MainActivity.java
@@ -1,11 +1,17 @@
 package com.growingcoder.spotifystreamer.search;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.growingcoder.spotifystreamer.R;
 import com.growingcoder.spotifystreamer.core.BaseActivity;
 import com.growingcoder.spotifystreamer.toptracks.TopTracksFragment;
+
+import java.util.Locale;
 
 /**
  * Displays a search field to search for artists on Spotify. It returns results in a list if there are any.
@@ -36,7 +42,7 @@ public class MainActivity extends BaseActivity {
                 artistFragment.setTopTracksFragment(topTracksFragment);
             }
 
-            if(getSupportActionBar() != null) {
+            if (getSupportActionBar() != null) {
                 getSupportActionBar().setSubtitle(savedInstanceState.getString(STATE_SUBTITLE));
             }
             return;
@@ -65,6 +71,50 @@ public class MainActivity extends BaseActivity {
                 outState.putString(STATE_SUBTITLE, subtitle.toString());
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.country:
+                showCountryPicker();
+                //TODO show dialog, handle rotation?
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showCountryPicker() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.country_dialog_title);
+
+        final Locale[] locales = Locale.getAvailableLocales();
+        CharSequence[] countries = new CharSequence[locales.length];
+        int counter = 0;
+        for (Locale locale : locales) {
+            countries[counter] = locale.getDisplayCountry();
+            counter++;
+        }
+
+        //TODO save and set checked item instead of -1
+
+        builder.setSingleChoiceItems(countries, -1,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        locales[item].getCountry();
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog countryDialog = builder.create();
+        countryDialog.show();
     }
 }
 
